a4f3e1ead01b76dd98009166fdd81911089276b9
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2017-06-07 23:25:58 +0530

    Update rule-engine.js

--- rule-engine.js
+++ rule-engine.js
@@ -4,12 +4,10 @@ function Rule(options){
   
 }
 
-
-
 Rule.prototype.run = function(facts){
     for(var key in this.conditions){
       if (this.conditions.hasOwnProperty(key)) {
-         return Rule.execCondition(key, this.conditions[key], facts);
+         return Rule.execConditions(key, this.conditions[key], facts);
       }
     }
   
@@ -17,49 +15,41 @@ Rule.prototype.run = function(facts){
 
 Rule.execExpression = function(expression, facts){
   var variable = expression.variable.split('.');
-  var lhs = facts[variable[0]][variable[1]];
-  
+  var lhs = facts[variable[0]][variable[1]];  
   if(lhs === expression.value){
      return true;
   }
   
 }
 
-Rule.execCondition = function(operator, conditions, facts){
+Rule.execCondition = function(condition, facts){
+  if(condition.hasOwnProperty('any')){
+      return Rule.execConditions('any', condition.any, facts);
+  } else if(condition.hasOwnProperty('all')){
+      return Rule.execConditions('all', condition.all, facts);
+  } else if(condition.hasOwnProperty('operator')) {
+      return Rule.execExpression(condition, facts);
+  }
+}
+
+Rule.execConditions = function(operator, conditions, facts){
     var result, condition;
     if(operator === 'any'){
       for(var i = 0; i < conditions.length; i++){
-        condition = conditions[i];
-        if(condition.hasOwnProperty('any')){
-            result = Rule.execCondition('any', condition['any'], facts);
-        } else if(condition.hasOwnProperty('all')){
-            result = Rule.execCondition('all', condition['all'], facts);
-        } else if(condition.hasOwnProperty('operator')) {
-          Rule.execExpression(expression, facts);
-        }
-        if(result){
-          return result;
+        if(Rule.execCondition(conditions[i], facts)){
+          return true;
         }
       }
       return false;
     } else if(operator === 'all'){
       for(var j = 0; j < conditions.length; j++){
-        condition = conditions[j];
-        if(condition.hasOwnProperty('any')){
-            result = Rule.execCondition('any', condition['any'], facts);
-        } else if(condition.hasOwnProperty('all')){
-            result = Rule.execCondition('all', condition['all'], facts);
-        } else if(condition.hasOwnProperty('operator')) {
-            result = Rule.execExpression(condition, facts);
-        }
-        if(!result){
-          return result;
+        if(!Rule.execCondition(conditions[j], facts)){
+          return false;
         }
       }
       return true;
     }
 }
-
 
 
 r = new Rule({
@@ -78,6 +68,6 @@ r = new Rule({
 
 console.log(r.run({
   student : {
-    age: 406
+    age: 40
   }
 }));
