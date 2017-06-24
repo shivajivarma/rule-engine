2ae02c0ae4e6835aa8291d21494f9c6d3f085fcc
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2017-06-24 18:14:57 +0530

    Update rule-engine.js

--- rule-engine.js
+++ rule-engine.js
@@ -101,23 +101,3 @@
 	global.Rule = Rule;
 
 })(this);
-
-r = new Rule({
-		conditions : {
-			any : [{
-					all : [{
-							lhs : ['student.age'],
-							operator : 'lessThan',
-							rhs : 40
-						}
-					]
-				}
-			]
-		}
-	});
-
-console.log(r.run({
-		student : {
-			age : 44
-		}
-	}));
